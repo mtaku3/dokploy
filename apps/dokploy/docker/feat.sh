82efa454419d65cd4e22f7e25bce05737dff6c3d
mtaku3 <me@mtaku3.com>
2025-05-23 15:20:23 +0900

    feat!: change docker dokploy/dokploy to mtaku3/dokploy

--- apps/dokploy/docker/feat.sh
+++ apps/dokploy/docker/feat.sh
@@ -5,6 +5,6 @@
 
 # docker buildx build --platform linux/amd64,linux/arm64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' --push .
 
-docker build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' .
+docker build --platform linux/amd64 --pull --rm -t "mtaku3/dokploy:feature" -f 'Dockerfile' .
 
 # docker  build --platform linux/amd64 --pull --rm -t "dokploy/dokploy:feature" -f 'Dockerfile' .
--- packages/server/src/services/settings.ts
+++ packages/server/src/services/settings.ts
@@ -22,7 +22,7 @@ export const getDokployImageTag = () => {
 };
 
 export const getDokployImage = () => {
-	return `dokploy/dokploy:${getDokployImageTag()}`;
+	return `mtaku3/dokploy:${getDokployImageTag()}`;
 };
 
 export const pullLatestRelease = async () => {
@@ -61,7 +61,7 @@ export const getUpdateData = async (): Promise<IUpdateData> => {
 		return DEFAULT_UPDATE_DATA;
 	}
 
-	const baseUrl = "https://hub.docker.com/v2/repositories/dokploy/dokploy/tags";
+	const baseUrl = "https://hub.docker.com/v2/repositories/mtaku3/dokploy/tags";
 	let url: string | null = `${baseUrl}?page_size=100`;
 	let allResults: { digest: string; name: string }[] = [];
 	while (url) {
